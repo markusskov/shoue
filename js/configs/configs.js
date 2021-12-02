b11c37ce23f7d4bbc2b44b65027c486e3f46341e
Markus Skov <Markusskov@hotmail.no>
2021-12-02 17:38:40 +0100

    testing

--- js/configs/configs.js
+++ js/configs/configs.js
@@ -3,9 +3,6 @@ export const BASE_URL = 'https://rocky-sierra-47757.herokuapp.com';
 export const header = {
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage
-      .getItem('jwt')
-      .replace(/^"(.*)"$/, '$1')}`,
+    Authorization: `Bearer ${localStorage.getItem('jwt')}`,
   },
 };
-console.log(header);
