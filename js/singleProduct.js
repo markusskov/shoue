7f45a2aaad412cb2817c8525dc40680d8ca354a1
Markus Skov <Markusskov@hotmail.no>
2021-12-02 21:07:15 +0100

    Added button to save products to local storage.

--- js/singleProduct.js
+++ js/singleProduct.js
@@ -1,14 +1,22 @@
 import { BASE_URL } from './configs/configs.js';
+import {
+  getFromLocalStorage,
+  saveToLocalStorage,
+} from './libs/localStorage.js';
 
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get('id');
 console.log(id);
 
-const productImage = document.querySelector('.singpleProduct__image');
+const productImage = document.querySelector('.singleProduct__image');
 const title = document.querySelector('.singleProduct__title');
 const subtitle = document.querySelector('.singleProduct__subtitle');
 const price = document.querySelector('.singleProduct__price');
+const aboutTitle = document.querySelector('.description__title');
+const aboutInfo = document.querySelector('.description__info');
+const featuresInfo = document.querySelector('.features__info');
+const addToCartBtn = document.querySelector('.addToCartBtn');
 
 async function displaySingleProduct() {
   const { data } = await axios.get(`${BASE_URL}/products/${id}`);
@@ -16,6 +24,38 @@ async function displaySingleProduct() {
   productImage.innerHTML = `<img class="img-fluid img__background" src="${data.img.url}">`;
   title.innerHTML = `${data.title}`;
   subtitle.innerHTML = `${data.subtitle}`;
-  price.innerHTML = `Price: $${data.price}`;
+  price.innerHTML = `Price: <b>$${data.price}</b>`;
+  aboutTitle.innerHTML = `About the ${data.title}`;
+  aboutInfo.innerHTML = `${data.about}`;
+  featuresInfo.innerHTML = `${data.product_features}`;
+  // Add data attributes to button for saving to local storage
+  addToCartBtn.dataset.id = `${data.id}`;
+  addToCartBtn.dataset.title = `${data.title}`;
+  addToCartBtn.dataset.url = `${data.img.url}`;
+  addToCartBtn.dataset.price = `${data.price}`;
 }
+function addToCart() {
+  addToCartBtn.onclick = () => {
+    let product = {
+      id: addToCartBtn.dataset.id,
+      title: addToCartBtn.dataset.title,
+      url: addToCartBtn.dataset.url,
+      price: addToCartBtn.dataset.price,
+    };
+    let savedProducts = getFromLocalStorage('savedProducts');
+    let findProduct = savedProducts.find((singleProduct) => {
+      return singleProduct.id === addToCartBtn.dataset.id;
+    });
+    if (findProduct === undefined) {
+      savedProducts.push(product);
+      saveToLocalStorage('savedProducts', savedProducts);
+    } else {
+      let removeSavedProduct = savedProducts.filter((singleProduct) => {
+        return singleProduct.id !== addToCartBtn.dataset.id;
+      });
+      saveToLocalStorage('savedProducts', removeSavedProduct);
+    }
+  };
+}
+addToCart();
 displaySingleProduct();
